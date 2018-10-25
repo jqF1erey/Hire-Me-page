3211c1c62b6bbd596a9e3e88b0e30af443d34867
jqF1erey <43240454+jqF1erey@users.noreply.github.com>
2018-10-25 08:10:22 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,4 @@
+//Things I still need,(delete them when you get it
+//At least one link to a website<technically>
+//An actual image of Brianna<done>
+//I'm not actually good at coding and making it look good so I'm sorry that it looks like garbage
